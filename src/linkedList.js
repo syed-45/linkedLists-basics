b4fbf104b74b3f93e0a2369ed16e91bdd84ba286
syed <user.email>
2022-10-17 10:48:45 +0100

    a

---
+++ src/linkedList.js
@@ -0,0 +1,18 @@
+node0 = {
+    value: 10,
+    pointer: node1
+}
+
+node1 = {
+    value: 20,
+    pointer: node2
+}
+node2 = {
+    value: 30,
+    pointer: node3
+}
+node3 = {
+    value: 40,
+    pointer: none
+}
+
--- src/skeleton.ts
+++ src/skeleton.ts
@@ -4,6 +4,45 @@
  * @param b - the second number to add
  * @returns the total
  */
+
+interface linkedList {
+  value : number;
+  pointer: linkedList | null
+}
+function firstLInkedListCreation (num1 = 10,num2 = 20,num3 = 30,num4 = 40): linkedList {
+  const returnList: linkedList = {
+    value: num1, pointer: {
+      value: num2, pointer:{
+        value: num3, pointer: {
+          value: num4, pointer: null
+        } 
+      }
+    }
+  }
+  return returnList
+}
+
+function createLinkedList(nums: number[]): linkedList{
+    let num  = nums[0]
+    const returnList: linkedList = {value: num, pointer: null}    
+    if (nums.length!==1) {    
+      returnList.pointer = createLinkedList(nums.slice(1,nums.length))
+    }
+    return returnList  
+}
+
+function traverseLinkedList(list: linkedList): void{
+  let tempList: linkedList | null = list;
+  while (tempList !== null) {
+    console.log(tempList.value);
+    tempList = tempList.pointer;
+  }
+}
+
+
+console.log(createLinkedList([90,80,60,40]));
+
+
 function sum(a: number, b: number): number {
   return a + b;
 }
